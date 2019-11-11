1109c37c904e43d4b6c98d691e73e04dae1a92c3
mariano <mariano@mariano-K46CA>
2019-11-11 14:17:51 -0200

    atualização classe fabricante

--- WebApplication4/src/java/br/com/fatecpg/db/Manufacturer.java
+++ WebApplication4/src/java/br/com/fatecpg/db/Manufacturer.java
@@ -18,45 +18,80 @@ import java.util.ArrayList;
 public class Manufacturer {
     private int id;
     private String name;
+    private String city;
+    private String state;
     private String email;
     
     
-    public static ArrayList<Manufacturer> getList() throws Exception{
-        ArrayList<Manufacturer> list = new ArrayList<>();
-        Class.forName("org.apache.derby.jdbc.ClientDriver");
-        String url = "jdbc:derby://localhost:1527/sample";
-        String user = "app";
-        String pass = "app";
-        Connection con = DriverManager.getConnection(url, user, pass);
-        Statement stmt = con.createStatement();
-        String sql = "SELECT MANUFACTURER_ID, NAME, EMAIL FROM MANUFACTURER";
-        ResultSet rs = stmt.executeQuery(sql);
-        while(rs.next()){
-            Manufacturer m = new Manufacturer(
-                    rs.getInt("MANUFATURER_ID"),
-                    rs.getString("NAME"),
-                    rs.getString("EMAIL")
-            );
+    public static ArrayList<Manufacturer> getList() throws Exception {
+    ArrayList<Manufacturer> list = new ArrayList<> ();
+    Class.forName("org.apache.derby.jdbc.ClientDriver");
+    String url = "jdbc:derby://localhost:1527/sample";
+    String user = "app";
+    String pass = "app";
+    Connection con = DriverManager.getConnection(url, user, pass);
+    Statement stmt = con.createStatement();
+    String sql = "SELECT MANUFACTURER_ID, NAME, CITY, STATE, EMAIL FROM MANUFACTURER";
+    ResultSet rs = stmt.executeQuery(sql);
+    while (rs.next()){
+        Manufacturer m = new Manufacturer (
+            rs.getInt("MANUFACTURER_ID")
+                    , rs.getString("NAME")
+                    , rs.getString("CITY")
+                    , rs.getString("STATE")
+                    , rs.getString("EMAIL")
+        
+        );
             list.add(m);
-        }
-        return list;
+    }
+    return list;
     }
 
-    public Manufacturer(int id, String name, String email) {
+    public Manufacturer(int id, String name, String city, String state, String email) {
         this.id = id;
         this.name = name;
+        this.city = city;
+        this.state = state;
         this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public int getId() {
+        return id;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+    
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
     }
 }
