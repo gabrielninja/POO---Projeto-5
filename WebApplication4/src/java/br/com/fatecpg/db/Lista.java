2f3888178d4d5d0ec1fb2b5a7df7acf45b936167
Gabriel <gabrielsprieto@gmail.com>
2019-11-10 22:35:04 -0200

    lista.java e lista_compras.jsp

--- WebApplication4/src/java/br/com/fatecpg/db/Lista.java
+++ WebApplication4/src/java/br/com/fatecpg/db/Lista.java
@@ -20,36 +20,40 @@ public class Lista {
     private int product_id;
     
    public static ArrayList<Lista> getList() throws Exception{
-        ArrayList<Lista> list = new ArrayList<>();
+        ArrayList<Lista> list = new ArrayList();
         Class.forName("org.apache.derby.jdbc.ClientDriver");
         String url = "jdbc:derby://localhost:1527/sample";
         String user = "app";
         String pass = "app";
         Connection con = DriverManager.getConnection(url, user, pass);
         Statement stmt = con.createStatement();
-        String sql = "SELECT P.CUSTUMER_ID, C.\"NAME\", PRO.PRODUCT_ID \n" +
-                "FROM PURCHASE_ORDER AS P\n" +
-                "INNER JOIN CUSTOMER AS C ON P.CUSTOMER_ID = C.CUSTOMER_ID\n" +
-                "INNER JOIN PRODUCT AS PRO ON P.PRODUCT_ID = PRO.PRODUCT_ID\n" +
-                "WHERE P.CUSTOMER_ID = "+customer_id+"";
+        String sql ="SELECT PRO.PRODUCT_ID, PRO.DESCRIPTION \n" +
+                    "FROM PURCHASE_ORDER AS P\n" +
+                    "INNER JOIN CUSTOMER AS C ON P.CUSTOMER_ID = C.CUSTOMER_ID\n" +
+                    "INNER JOIN PRODUCT AS PRO ON P.PRODUCT_ID = PRO.PRODUCT_ID\n" +
+                    "WHERE P.CUSTOMER_ID = "+customer_id+"";
         ResultSet rs = stmt.executeQuery(sql);
-        while(rs.next()){
-            Lista l = new Lista(
-                    rs.getInt("PRODUCT_ID"),
-                    rs.getInt("CUSTOMER_ID")         
-            );
-            list.add(l);
-        }
+        while (rs.next()) {
+           Lista lis  = new Lista (
+                    rs.getInt("CUSTOMER_ID"),
+                    rs.getInt("PRODUCT_ID")
 
-    public Lista(int customer_id) {
-        this.customer_id = customer_id;
+            );
+            list.add(lis);
+        }
+        return list;
     }
 
-    public int getCliente_id() {
+    public Lista (int customer_id, int product_id) {
+        this.customer_id = customer_id;
+        this.product_id = product_id;
+    }
+
+    public int getCustomer_id() {
         return customer_id;
     }
 
-    public void setCliente_id(int customer_id) {
+    public void setCustomer_id(int customer_id) {
         this.customer_id = customer_id;
     }
 
--- WebApplication4/web/lista_compras.jsp
+++ WebApplication4/web/lista_compras.jsp
@@ -3,16 +3,33 @@
     Created on : Nov 10, 2019, 8:24:24 PM
     Author     : gabrielprieto
 --%>
-
+<%@page import="br.com.fatecpg.db.Lista"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Lista de Compras</title>
+        <title>Compras</title>
     </head>
     <body>
-        <h1>Lista</h1>
-        
+        <%@include file="WEB-INF/jspf/navbar.jspf"%>
+        <%try{%>
+        <table border="1">
+            <tr> 
+                <a href="lista_compras.jsp">Compras</a></h2>
+                <th>Id</th>
+                
+
+            </tr>
+           <%for(Lista lis: Lista.getList(String.Parse(getParameter("customer_id")))){%>
+            <tr >
+                <td><%= lis.getCustomer_id()%></td>
+               
+            </tr>
+            <%}%>
+        </table> 
+        <%}catch (Exception ex){%>
+            <tr><td colspan="5"><%= ex.getMessage()%></td></tr>
+        <%}%>
     </body>
 </html>
